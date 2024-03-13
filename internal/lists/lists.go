0ec55545d790a4bb084e37e876a2cdeca0036683
Vyr Cossont <VyrCossont@users.noreply.github.com>
2024-03-13 16:24:15 -0700

    lists: implement import

--- internal/lists/lists.go
+++ internal/lists/lists.go
@@ -17,12 +17,202 @@
 
 package lists
 
-import "github.com/VyrCossont/slurp/internal/auth"
+import (
+	"log/slog"
+	"strings"
+
+	"github.com/go-openapi/runtime"
+	"github.com/pkg/errors"
+
+	"github.com/VyrCossont/slurp/client/lists"
+	"github.com/VyrCossont/slurp/internal/auth"
+	"github.com/VyrCossont/slurp/internal/resolve"
+	"github.com/VyrCossont/slurp/internal/util"
+	"github.com/VyrCossont/slurp/models"
+)
+
+// Note: Mastodon's list list exports currently don't have a CSV header.
 
 func Export(authClient *auth.Client, file string) error {
 	return nil
 }
 
 func Import(authClient *auth.Client, file string) error {
+	csvRows, err := util.ReadCSV(file)
+	if err != nil {
+		return err
+	}
+
+	// Collect set of all accounts so we can resolve them only once each.
+	listAccountAddressesSet := map[string]interface{}{}
+	// Collect list of accounts in each list.
+	listAccountAddressesByTitle := map[string][]string{}
+	for _, row := range csvRows {
+		listListEntry, err := newListListEntryFromCsvFields(row)
+		if err != nil {
+			slog.Warn("couldn't parse list entry from CSV row", "row", row, "error", err)
+			continue
+		}
+		listAccountAddressesSet[listListEntry.accountAddress] = nil
+		listAccountAddressesByTitle[listListEntry.title] = append(
+			listAccountAddressesByTitle[listListEntry.title],
+			listListEntry.accountAddress,
+		)
+	}
+
+	// Resolve all accounts.
+	listAccountIDsByAddress := map[string]string{}
+	for accountAddress := range listAccountAddressesSet {
+		account, err := resolve.Account(authClient, "@"+accountAddress)
+		if err != nil {
+			slog.Warn("couldn't resolve account", "account_address", accountAddress, "error", err)
+			continue
+		}
+		listAccountIDsByAddress[accountAddress] = account.ID
+	}
+
+	// Fetch existing lists.
+	existingListIDsByTitle := map[string]string{}
+
+	err = authClient.Wait()
+	if err != nil {
+		return err
+	}
+
+	resp, err := authClient.Client.Lists.Lists(nil, authClient.Auth)
+	if err != nil {
+		slog.Warn("couldn't fetch existing lists", "error", err)
+		// We can technically keep going without them, but it really should succeed.
+	} else {
+		for _, list := range resp.GetPayload() {
+			existingListIDsByTitle[list.Title] = list.ID
+		}
+	}
+
+	// Create each list if necessary and add accounts to them.
+	for title, listAccountAddresses := range listAccountAddressesByTitle {
+		var listID string
+		var found bool
+		if listID, found = existingListIDsByTitle[title]; !found {
+			// Create a new list.
+			err := authClient.Wait()
+			if err != nil {
+				return err
+			}
+
+			resp, err := authClient.Client.Lists.ListCreate(
+				&lists.ListCreateParams{
+					Title: title,
+				},
+				authClient.Auth,
+				func(op *runtime.ClientOperation) {
+					op.ConsumesMediaTypes = []string{"application/x-www-form-urlencoded"}
+				},
+			)
+			if err != nil {
+				slog.Warn("couldn't create list", "title", title, "error", err)
+				continue
+			}
+			listID = resp.GetPayload().ID
+		}
+
+		// Add list accounts one by one.
+		// The Mastodon API specifies that adding accounts to a list is not idempotent,
+		// and will fail if the same account is already there. GtS is flaw-compatible.
+		for _, accountAddress := range listAccountAddresses {
+			var accountID string
+			if accountID, found = listAccountIDsByAddress[accountAddress]; !found {
+				// We've already logged a warning for not being able to resolve this account.
+				continue
+			}
+			params := &lists.AddListAccountsParams{
+				ID:         listID,
+				AccountIds: []string{accountID},
+			}
+
+			err := authClient.Wait()
+			if err != nil {
+				return err
+			}
+
+			_, err = authClient.Client.Lists.AddListAccounts(
+				params,
+				authClient.Auth,
+				func(op *runtime.ClientOperation) {
+					op.ConsumesMediaTypes = []string{"application/x-www-form-urlencoded"}
+				},
+			)
+			if err != nil {
+				slog.Warn("couldn't add account to list", "title", title, "list_id", listID, "account_id", accountID, "error", err)
+				continue
+			}
+		}
+	}
+
 	return nil
+}
+
+// listAccountsPagedRequester has no state.
+type listAccountsPagedRequester struct {
+}
+
+func (pagedRequester *listAccountsPagedRequester) Request(authClient *auth.Client, maxID *string) (*listAccountsPagedResponse, error) {
+	resp, err := authClient.Client.Lists.ListAccounts(nil, authClient.Auth)
+	if err != nil {
+		return nil, err
+	}
+	return &listAccountsPagedResponse{resp}, nil
+}
+
+type listAccountsPagedResponse struct {
+	resp *lists.ListAccountsOK
+}
+
+func (pagedResponse *listAccountsPagedResponse) Link() string {
+	return pagedResponse.resp.Link
+}
+
+func (pagedResponse *listAccountsPagedResponse) Elements() []*models.Account {
+	return pagedResponse.resp.GetPayload()
+}
+
+type listListEntry struct {
+	// title is repeated as many times as there are accounts in a list.
+	title          string
+	accountAddress string
+}
+
+func newListListEntry(ownDomain string, list *models.List, account *models.Account) *listListEntry {
+	e := &listListEntry{
+		title:          list.Title,
+		accountAddress: account.Acct,
+	}
+	if !strings.ContainsRune(e.accountAddress, '@') {
+		e.accountAddress += "@" + ownDomain
+	}
+	return e
+}
+
+func (e *listListEntry) csvFields() []string {
+	return []string{
+		e.title,
+		e.accountAddress,
+	}
+}
+
+func newListListEntryFromCsvFields(fields []string) (*listListEntry, error) {
+	e := &listListEntry{}
+
+	if len(fields) < 2 {
+		return nil, errors.WithStack(errors.New("not enough fields, expected at least 2"))
+	}
+	e.title = fields[0]
+	e.accountAddress = fields[1]
+	if !strings.ContainsRune(e.accountAddress, '@') {
+		err := errors.WithStack(errors.New("account address expected to contain @"))
+		slog.Error("malformed account address", "fields", fields)
+		return nil, err
+	}
+
+	return e, nil
 }
